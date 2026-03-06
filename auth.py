00e67601527fe43b1fb490f32352651c0d19f24e
Shion <nhan19092006@gmail.com>
2026-03-06 23:09:33 +0700

    authentication 1/2

---
+++ auth.py
@@ -0,0 +1,18 @@
+from datetime import UTC, datetime, timedelta
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+from config import settings
+
+password_hash = PasswordHash.recommended()
+
+oath2_scheme = OAuth2PasswordBearer(tokenUrl="api/users/token")
+
+def hash_password(password:str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password:str, hashed_password:str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+def create_access_token():
+    pass
